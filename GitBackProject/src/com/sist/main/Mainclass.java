7c7c5346c46261db37b282f37707aa9fff75c29b
myspacekjy <myspacekjy@gmail.com>
2022-07-04 18:26:34 +0900

    back_2

---
+++ GitBackProject/src/com/sist/main/Mainclass.java
@@ -0,0 +1,13 @@
+package com.sist.main;
+
+public class Mainclass {
+	public Mainclass() {
+		// TODO Auto-generated constructor stub
+	System.out.println("git");
+	System.out.println("git-2");
+	System.out.println("git-3");
+	}
+	
+	
+	
+}
